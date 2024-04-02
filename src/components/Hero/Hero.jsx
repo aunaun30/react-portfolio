567f36166948acb914f62907ae399d9643ae6771
aunaun30 <152370449+aunaun30@users.noreply.github.com>
2024-04-03 00:16:07 +0700

    update50%
    update50%

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,13 +7,12 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Gade</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a Front-End Developer , Back-End Developer and UX/UI Designer
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
-          Contact Me
+        <a href="https://drive.google.com/file/d/1jI14J7cnSeXXuhjoFxGnLcSRw1uOG0di/view?usp=sharing" className={styles.contactBtn}>
+          Downloads CV
         </a>
       </div>
       <img
